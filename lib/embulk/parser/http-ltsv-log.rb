8fe75d9d5e1d1531395732069a956c32822e0c11
Hirokazu Hata <h.hata.ai.t@gmail.com>
2015-08-03 01:05:02 +0900

    can parse log

--- lib/embulk/parser/http-ltsv-log.rb
+++ lib/embulk/parser/http-ltsv-log.rb
@@ -2,48 +2,67 @@ require 'date'
 
 module Embulk
   module Parser
-
     class HttpLtsvLog < ParserPlugin
-      Plugin.register_parser("http-ltsv-log", self)
+      Plugin.register_parser('http-ltsv-log', self)
 
-      def self.transaction(config, &control)
-
+      def self.transaction(config, &_control)
+        parser_task = config.load_config(Java::LineDecoder::DecoderTask)
         task = {
-          # "skip_header_lines" => config.param("skip_header_lines", :boolean, default: true)
+          'decoder_task' => DataSource.from_java(parser_task.dump),
+          'schema' => config.param('schema', :array)
         }
-
-        columns = config.param("columns", :array).map.with_index do |c, i|
-          if ['boolean', 'long', 'timestamp', 'double', 'string'].include?(c['type'])
-            if c['type'] == 'timestamp'
-              col = Column.new(i, DateTime.parse(c['name']), c['type'].to_sym) if c['format'].nil?
-              col =  Column.new(i, DateTime.parse(c['name'], c['format']) , c['type'].to_sym)
-            else
-              col = Column.new(i, c['name'], c['type'].to_sym)
-            end
-          else
-            raise 'Wrong column type'
-          end
-          col
+        columns = task['schema'].each_with_index.map do |c, i|
+          Column.new(i, c['name'], c['type'].to_sym)
         end
-
         yield(task, columns)
       end
 
       def init
-        # @skip_header_lines = task["skip_header_lines"]
+        @decoder_task = task.param('decoder_task', :hash).load_task(Java::LineDecoder::DecoderTask)
       end
 
       def run(file_input)
-        decoder_task = @task.load_config(Java::LineDecoder::DecoderTask)
-        decoder = Java::LineDecoder.new(file_input.instance_eval { @java_file_input }, decoder_task)
+        decoder = Java::LineDecoder.new(file_input.instance_eval { @java_file_input }, @decoder_task)
+        schema = @task['schema']
+
         while decoder.nextFile
           while line = decoder.poll
-            record = nil
-            record = line.split("\t").map{ |column| column.gsub(/^[^:]*:/,'')}
-            page_builder.add(record)
+            begin
+              hash = Hash[line.split("\t").map { |f| f.split(':', 2) }]
+              @page_builder.add(make_record(schema, hash))
+            rescue
+              # TODO: logging
+            end
           end
         end
         page_builder.finish
+      end
+
+      private
+
+      def make_record(schema, e)
+        schema.map do |c|
+          val = e[c['name']]
+          v = val.nil? ? '' : val
+          case c['type']
+          when 'string'
+            v
+          when 'long'
+            v.to_i
+          when 'double'
+            v.to_f
+          when 'boolean'
+            %w(yes true 1).include?(v.downcase)
+          when 'timestamp'
+            if c['surrounded'] == true
+              v.empty? ? nil : Time.strptime(v.gsub(/(^.|.$)/, ''), c['format'])
+            else
+              v.empty? ? nil : Time.strptime(v, c['format'])
+            end
+          else
+            fail "Unsupported type #{c['type']}"
+          end
+        end
       end
     end
   end
